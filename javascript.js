6e30429a4988296386d7605f717d7be8c1a4d296
Juanjhenao <juanjosehenao20@gmail.com>
2022-05-27 13:56:53 -0500

    functionality to modify the grid to taste and draw by hovering

--- javascript.js
+++ javascript.js
@@ -1,12 +1,38 @@
 const container = document.querySelector('.container');
+let cuadrados = 16*16;
 
-for (i = 1; i <= 256; i++){
-    let div = document.createElement('div');
-    // div.classList.add('block-' + i );
-    div.classList.add('pixel')
-    div.onmouseover = ()=> {
-    div.classList.add('black')
+const slider = document.getElementById('slider');
+const textSize = document.getElementById('pixelSize')
+
+
+slider.oninput = ()=> {
+    textSize.textContent = `${slider.value} x ${slider.value}`
+};
+
+function gridSetup (){
+    container.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`
+}
+
+function generatedivs (){
+    
+    gridSetup();
+
+    for (i = 1; i <= cuadrados; i++){
+        let div = document.createElement('div');
+        div.classList.add('pixel')
+        div.onmouseover = ()=> {
+        div.classList.add('black')
+        };
+        container.appendChild(div)
+     }
     };
 
-    container.appendChild(div)
- }
+
+generatedivs();
+
+slider.onchange = ()=> {
+    container.innerHTML = ''
+    cuadrados = slider.value*slider.value
+    generatedivs();
+};
